df50ceaf79ebad72b7be5b1e42b87aa534f37680
GregorySchwing <gschwing@uno.edu>
2022-04-18 19:28:52 -0500

    add some couts

--- src/WolfCalibrationOutput.cpp
+++ src/WolfCalibrationOutput.cpp
@@ -176,6 +176,10 @@ void WolfCalibrationOutput::DoOutput(const ulong step) {
                         // So there are no duplicate columns.
                         for (int r = 1; r < statValRef.forcefield.numberOfRCuts[box]; ++r){
                               for (int a = 1; a < statValRef.forcefield.numberOfAlphas[box]; ++a){
+                                    std::cout << "abs(ewaldRef.boxEnergy[box].total) " << abs(ewaldRef.boxEnergy[box].total) << std::endl;
+                                    std::cout << "abs(electrostaticEnergies[box][wolfKind][coulKind][r][a]) " << abs(electrostaticEnergies[box][wolfKind][coulKind][r][a])<< std::endl;
+                                    std::cout << "(abs(ewaldRef.boxEnergy[box].total) -  abs(electrostaticEnergies[box][wolfKind][coulKind][r][a]))/ abs(ewaldRef.boxEnergy[box].total) " << (abs(ewaldRef.boxEnergy[box].total) -  abs(electrostaticEnergies[box][wolfKind][coulKind][r][a]))/ abs(ewaldRef.boxEnergy[box].total)<< std::endl;
+                                    std::cout << "GetString((abs(ewaldRef.boxEnergy[box].total) -  abs(electrostaticEnergies[box][wolfKind][coulKind][r][a]))/ abs(ewaldRef.boxEnergy[box].total), 8) "<< GetString((abs(ewaldRef.boxEnergy[box].total) -  abs(electrostaticEnergies[box][wolfKind][coulKind][r][a]))/ abs(ewaldRef.boxEnergy[box].total), 8) << std::endl;
                                     row += GetString((abs(ewaldRef.boxEnergy[box].total) -  abs(electrostaticEnergies[box][wolfKind][coulKind][r][a]))/ abs(ewaldRef.boxEnergy[box].total), 8);
                                     row += "\t";
                               }
