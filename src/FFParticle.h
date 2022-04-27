725a92416e36f41803dad12168c76931c9c28de5
GregorySchwing <gschwing@uno.edu>
2022-04-27 16:30:07 -0500

    CalcCoul and CalCoulVir done

--- src/FFParticle.h
+++ src/FFParticle.h
@@ -75,12 +75,15 @@ public:
   // coulomb interaction functions
   virtual double CalcCoulomb(const double distSq, const uint kind1,
                              const uint kind2, const double qi_qj_Fact,
-                             const double lambda, const uint b, 
+                             const double lambda, const uint b,
                              double rCutCoulomb,
+                             double rCutCoulombSq,
+                             double wolfFactor1, 
+                             double wolfFactor2,
                              double wolfAlpha) const;
   virtual double CalcCoulombVir(const double distSq, const uint kind1,
                                 const uint kind2, const double qi_qj,
-                                const double lambda, uint b, 
+                                const double lambda, const uint b,
                                 double rCutCoulombSq,
                                 double wolfFactor2,
                                 double wolfFactor3,
