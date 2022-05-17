0a131f5cf88cd3929ffe39e0387119357e3e510c
GregorySchwing <gschwing@uno.edu>
2022-05-17 17:29:22 -0500

    right var

--- src/GPU/CalculateForceCUDAKernel.cu
+++ src/GPU/CalculateForceCUDAKernel.cu
@@ -321,8 +321,8 @@ void CallBoxForceGPU(VariablesCUDA *vars,
   double cpu_final_REn = 0.0, cpu_final_LJEn = 0.0;
 
   threadsPerBlock = 256;
-  blocksPerGrid = numberOfCells * NUMBER_OF_NEIGHBOR_CELLS;
-  energyVectorLen = numberOfCells * NUMBER_OF_NEIGHBOR_CELLS * threadsPerBlock;
+  blocksPerGrid = numberOfCells;
+  energyVectorLen = numberOfCells * threadsPerBlock;
 
   CUMALLOC((void**) &gpu_LJEn, energyVectorLen * sizeof(double));
   CUMALLOC((void**) &gpu_final_LJEn, sizeof(double));
