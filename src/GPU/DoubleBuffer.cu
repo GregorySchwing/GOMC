13d6193279aea90deadc5d1a7dbf20eb7298515f
GregorySchwing <gschwing@uno.edu>
2022-05-18 12:04:54 -0500

    t

--- src/GPU/DoubleBuffer.cu
+++ src/GPU/DoubleBuffer.cu
@@ -7,11 +7,11 @@ along with this program, also can be found at <https://opensource.org/licenses/M
 #include "DoubleBuffer.cuh"
 #ifdef GOMC_CUDA
 
-template<typename T> DoubleBuffer::DoubleBuffer<T>(int arraySize) {
+template<typename T> DoubleBuffer<T>::DoubleBuffer(int arraySize) {
     CUMALLOC((void**) array1, arraySize * sizeof(T));
     CUMALLOC((void**) array2, arraySize * sizeof(T));
 }
-template<typename T> DoubleBuffer::~DoubleBuffer<T>() {
+template<typename T> DoubleBuffer<T>::~DoubleBuffer() {
     CUFREE(array1);
     CUFREE(array2);
 }
