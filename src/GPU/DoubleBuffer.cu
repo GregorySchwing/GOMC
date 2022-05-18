90054e3df372bb93055d626735f08f2cf6c2931f
GregorySchwing <gschwing@uno.edu>
2022-05-18 12:53:17 -0500

    use bt

--- src/GPU/DoubleBuffer.cu
+++ src/GPU/DoubleBuffer.cu
@@ -9,17 +9,17 @@ along with this program, also can be found at <https://opensource.org/licenses/M
 #include "DoubleBuffer.cuh"
 #ifdef GOMC_CUDA
 
-template<typename T> DoubleBuffer<T>::DoubleBuffer<T>(int arraySize) {
+template<typename Buf_Type> DoubleBuffer<Buf_Type>::DoubleBuffer<Buf_Type>(int arraySize) {
     CUMALLOC((void**) selector, 2 * sizeof(void*));
-    CUMALLOC((void**) array1, arraySize * sizeof(T));
-    CUMALLOC((void**) array2, arraySize * sizeof(T));
+    CUMALLOC((void**) array1, arraySize * sizeof(Buf_Type));
+    CUMALLOC((void**) array2, arraySize * sizeof(Buf_Type));
     
     cudaMemcpy(&selector[0], array1, 1 * sizeof(void*),
     cudaMemcpyDeviceToDevice);
     cudaMemcpy(&selector[1], array2, 1 * sizeof(void*),
     cudaMemcpyDeviceToDevice);
 }
-template<typename T> DoubleBuffer<T>::~DoubleBuffer<T>() {
+template<typename Buf_Type> DoubleBuffer<Buf_Type>::~DoubleBuffer<Buf_Type>() {
     CUFREE(array1);
     CUFREE(array2);
 }
