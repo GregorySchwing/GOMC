d38f46e51a49900bda76a5902fa38a097cd18fc3
greg <go2432@wayne.edu>
2022-06-07 12:14:25 -0500

    fu

--- src/GPU/HelperFunctionsCUDA.cuh
+++ src/GPU/HelperFunctionsCUDA.cuh
@@ -3,9 +3,10 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
   
-  inline double LengthSq(double3 & vec) const
+  inline __device__ double LengthSq(double3 & vec) const
   {
     return vec.x * vec.x + vec.y * vec.y + vec.z * vec.z;
   }
+  
 
 #endif
--- src/GPU/MetropolisCriterionCUDA.cu
+++ src/GPU/MetropolisCriterionCUDA.cu
@@ -27,9 +27,9 @@ __global__ void GetCoeffTranslation(
                             double * molForceRecRefZ,
                             double * molForceRecNewX,
                             double * molForceRecNewY,
-                            double * molForceRecNewZ,
+                            double * molForceRecNewZ
                         ){
-          forceReal =  make_double3(0.0, 0.0, 0.0);
+          //forceReal =  make_double3(0.0, 0.0, 0.0);
 
 }
 
@@ -38,16 +38,16 @@ __device__ double CalculateWRatio(  const double3 &lb_new,
                                     const double3 &k,
                                     const double max4){
     double w_ratio = 0.0;
-    double3 old_var = lb_old - k;
-    double3 new_var = lb_new + k;
+    //double3 old_var = lb_old - k;
+    //double3 new_var = lb_new + k;
 
     //Note: we could factor max4 and multiply at the end, but
     //      for the move, where we translate and rotate all molecules,
     //      this method would not work. Hence, I did not factor it.
     // its actually is w_ratio += -1.0* but we simplify it
-    w_ratio -= (LengthSq(new_var) / max4);
+    //w_ratio -= (LengthSq(new_var) / max4);
     // its actually is w_ratio -= -1.0* but we simplify it
-    w_ratio += (LengthSq(old_var) / max4);
+    //w_ratio += (LengthSq(old_var) / max4);
 
     return w_ratio;
 }
