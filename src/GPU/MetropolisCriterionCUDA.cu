34bb5b6a260688d29e1eceeb597cf5ed4d854156
greg <go2432@wayne.edu>
2022-06-14 12:37:50 -0500

    remove last mult

--- src/GPU/MetropolisCriterionCUDA.cu
+++ src/GPU/MetropolisCriterionCUDA.cu
@@ -91,7 +91,7 @@ __global__ void GetCoeffTranslation(
 
     double3 k = make_double3   (t_k_x[molNumber],t_k_y[molNumber],t_k_z[molNumber]);
 
-    w_ratio += CalculateWRatio(bf_new, bf_old, k, t_max4) * BETA[0] * t_max[0];
+    w_ratio += CalculateWRatio(bf_new, bf_old, k, t_max4);
 
     atomicAdd(&mp_coefficient[0], w_ratio);
 
