83b54da50535ada7602853e6193527232189b6ce
greg <go2432@wayne.edu>
2022-08-15 18:00:45 -0500

    a

--- src/GPU/CellListGPU.cu
+++ src/GPU/CellListGPU.cu
@@ -325,10 +325,10 @@ __global__ void MapParticlesToCellKernel(
         printf("gpu_molLookup %d\n", gpu_molLookup[1]);
         printf("gpu_startAtomIdx %d\n", gpu_startAtomIdx[1]);
     }
+    __syncthreads();
     int molIndex = gpu_molLookup[warpIdx];
     uint b = molIndex < gpu_molBoxCount[0];
 
-    __syncthreads();
     //printf("b %d", b);
     for (int particleIndex = gpu_startAtomIdx[molIndex] + laneIdx; particleIndex < gpu_startAtomIdx[molIndex + 1]; particleIndex += warp_size ){
         int cell = PositionToCell(particleIndex,
