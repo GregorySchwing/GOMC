a5861fff000766c8ea4052fc3413bc9585a70a2f
greg <go2432@wayne.edu>
2022-08-15 12:23:29 -0500

    fix loop

--- src/GPU/CellListGPU.cu
+++ src/GPU/CellListGPU.cu
@@ -297,12 +297,13 @@ __global__ void MapParticlesToCellKernel(int molCount,
                             double **gpu_Invcell_z){
     int threadID = blockIdx.x * blockDim.x + threadIdx.x;
     // Optimal alu usage/memory latency will probably be 1 warp/molecule
-    uint molIndex = threadID / warp_size;
+    uint warpIdx = threadID / warp_size;
+    uint laneIdx = threadID % warp_size;
     if (molIndex >= molCount)
         return;
-    uint mol = gpu_molLookup[molIndex];
+    uint molIndex = gpu_molLookup[warpIdx];
     uint b = molIndex < gpu_molBoxCount[0];
-    for (int particleIndex = gpu_startAtomIdx[mol]; particleIndex < gpu_startAtomIdx[mol + 1]; particleIndex += warp_size ){
+    for (int particleIndex = gpu_startAtomIdx[molIndex] + laneIdx; particleIndex < gpu_startAtomIdx[molIndex + 1]; particleIndex += warp_size ){
         int cell = PositionToCell(particleIndex,
                                 gpu_x,
                                 gpu_y,
