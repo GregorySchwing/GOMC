928c9d6efebbabbcab8d00e419cd6c85ab1c6027
greg <go2432@wayne.edu>
2022-08-16 13:37:35 -0500

    print diff

--- test/src/CellListGPUTest.cpp
+++ test/src/CellListGPUTest.cpp
@@ -24,6 +24,19 @@ TEST(CellListGPU, CheckMETHANOL) {
                         mapParticleToCellGPU,
                         neighborListGPU,
                         particleIndices);
+    for (int i = 0; mapParticleToCell.size(); ++i){
+        if(mapParticleToCell[i] != mapParticleToCellGPU[i])
+            printf("%d %d\n", mapParticleToCell[i], mapParticleToCellGPU[i]);
+    }
+    for (int i = 0; cellStartIndex.size(); ++i){
+        if(cellStartIndex[i] != cellStartIndexGPU[i])
+            printf("%d %d\n", cellStartIndex[i], cellStartIndexGPU[i]);
+    }
+    for (int i = 0; cellVector.size(); ++i){
+        if(cellVector[i] != cellVectorGPU[i])
+            printf("%d %d\n", cellVector[i], cellVectorGPU[i]);
+    }
+
     EXPECT_EQ(mapParticleToCell, mapParticleToCellGPU);
     EXPECT_EQ(cellStartIndex, cellStartIndexGPU);
     EXPECT_EQ(cellVector, cellVectorGPU);
