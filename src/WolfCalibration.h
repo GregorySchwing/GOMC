e601d2f3de6c606192674b3d3ca2c2a28334cb9c
go2432 <go2432@wayne.edu>
2022-10-12 14:18:45 -0500

    dont calibrate hybrd

--- src/WolfCalibration.h
+++ src/WolfCalibration.h
@@ -41,7 +41,8 @@ private:
   int startOfWolfFactor[BOX_TOTAL];
   int numberOfRCuts[BOX_TOTAL];
   int numberOfAlphas[BOX_TOTAL];
-  std::string wolfKindStrings[4] = {"HYBRID", "VLUGT", "GROSS", "VLUGTWINTRACUTOFF"};
+  //std::string wolfKindStrings[4] = {"HYBRID", "VLUGT", "GROSS", "VLUGTWINTRACUTOFF"};
+  std::string wolfKindStrings[4] = {"VLUGT", "GROSS", "VLUGTWINTRACUTOFF"};
   std::string coulKindStrings[2] = {"DSP", "DSF"};
   // Wolf Calibration
 };
