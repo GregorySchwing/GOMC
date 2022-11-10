b580bbc33600ae2dd460af2588b57d16dba4bffd
greg <go2432@wayne.edu>
2022-11-10 15:07:19 -0600

    same fix for force

--- src/GPU/CalculateForceCUDAKernel.cu
+++ src/GPU/CalculateForceCUDAKernel.cu
@@ -169,8 +169,8 @@ void CallBoxInterForceGPU(VariablesCUDA *vars,
   } else {
     ewald_ptr = vars->gpu_ewald;
     wolf_ptr = vars->gpu_wolf;
-    rCutCoulomb_ptr = &vars->gpu_rCutCoulomb[box];
-    rCutCoulombSq_ptr = &vars->gpu_rCutCoulombSq[box];    
+    rCutCoulomb_ptr = vars->gpu_rCutCoulomb;
+    rCutCoulombSq_ptr = vars->gpu_rCutCoulombSq;    
   }
 
   double3 axis = make_double3(boxAxes.GetAxis(box).x,
