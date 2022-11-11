3651ff82442ed878d23804d0f6c01e3101696ef0
greg <go2432@wayne.edu>
2022-11-11 15:15:45 -0600

    builds

--- src/OutputVars.cpp
+++ src/OutputVars.cpp
@@ -124,7 +124,11 @@ void OutputVars::CalcAndConvert(ulong step)
     if (pressureCalc) {
       if((step + 1) % pCalcFreq == 0 || step == 0) {
         if(step != 0) {
-          virialRef[b] = calc.VirialCalc(b);
+          virialRef[b] = calc.VirialCalc(b, ffRef.rCutCoulomb[b], 
+                                            ffRef.rCutCoulombSq[b], 
+                                            ffRef.wolfFactor2[b],
+                                            ffRef.wolfFactor3[b],
+                                            ffRef.wolfAlpha[b]);
           *virialTotRef += virialRef[b];
         }
         //calculate surface tension in mN/M
