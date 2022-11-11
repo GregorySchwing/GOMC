8c1e73ab0bae892d03558b197a182cb1c10b8ee9
greg <go2432@wayne.edu>
2022-11-11 03:37:36 -0600

    fix correction term

--- src/Wolf.cpp
+++ src/Wolf.cpp
@@ -290,7 +290,7 @@ double Wolf::MolCorrection(uint molIndex, uint box,
       dampenedCorr = 0.0;
       if(distSq < rCutCoulombSq){
         // All methods have this constant term.
-        dampenedCorr -= wolfFactor1;
+        dampenedCorr -= wolfFactor1 * 0.5;
         if (isVlugtWolf || isVlugtWithIntraCutoffWolf){
           dist = sqrt(distSq);
           dampenedCorr += -1.0*erf(wolfAlpha * dist)/dist;   
@@ -489,7 +489,7 @@ double Wolf::SwapCorrection(const cbmc::TrialMol& trialMol,
       dampenedCorr = 0.0;
       if(distSq < rCutCoulombSq){
         // All methods have this constant term.
-        dampenedCorr -= wolfFactor1;
+        dampenedCorr -= wolfFactor1 * 0.5;
         if (isVlugtWolf || isVlugtWithIntraCutoffWolf){
           dist = sqrt(distSq);
           dampenedCorr += -1.0*erf(wolfAlpha * dist)/dist;   
@@ -666,7 +666,7 @@ void Wolf::ChangeCorrection(Energy *energyDiff, Energy &dUdL_Coul,
       dampenedCorr = 0.0;
       if(distSq < rCutCoulombSq){
         // All methods have this constant term.
-        dampenedCorr -= wolfFactor1;
+        dampenedCorr -= wolfFactor1 * 0.5;
         if (isVlugtWolf || isVlugtWithIntraCutoffWolf){
           dist = sqrt(distSq);
           dampenedCorr += -1.0*erf(wolfAlpha * dist)/dist;   
